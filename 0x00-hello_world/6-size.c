bb250c5f8fac7426281eaa54f7fb558637b22126
inioluwa-kidat001 <inioluwa-kidat001@gmail.com>
2022-10-13 18:06:24 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,9 @@
  */
 int main(void)
 {
-	printf("Size of char is %lu bytes(s)\n");
-	printf("Size of an int is %lu byte(s)\n");
-	printf("Size of long int is %lu byte(s)\n");
-	printf("Size of float is %lu byte(s)\n");
+	printf("Size of char is %lu bytes(s)\n", size of (char));
+	printf("Size of an int is %lu byte(s)\n", size of (int));
+	printf("Size of long int is %lu byte(s)\n", size of (long int));
+	printf("Size of float is %lu byte(s)\n", size of (float));
 	return (0);
 }
