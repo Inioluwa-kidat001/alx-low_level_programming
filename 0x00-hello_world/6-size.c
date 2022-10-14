773c98cd3959659efa7dc9c4c9c421558b78c472
inioluwa-kidat001 <inioluwa-kidat001@gmail.com>
2022-10-13 17:51:39 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of char is %lu bytes(s)\n");
+	printf("Size of an int is %lu byte(s)\n");
+	printf("Size of long int is %lu byte(s)\n");
+	printf("Size of float is %lu byte(s)\n");
+	return (0);
+}
