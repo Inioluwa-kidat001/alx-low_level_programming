5f12e9a55c540a5c638f5552f330ed3c500dedf8
inioluwa-kidat001 <inioluwa-kidat001@gmail.com>
2022-10-16 16:08:44 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program entty
+ *Return:0
+ **/
+int main(void)
+{
+	char t;
+
+	for (t = 'a'; t <= 'z'; t++)
+	{
+		putchar(t);
+	}
+	putchar("\n");
+	return (0);
+}
