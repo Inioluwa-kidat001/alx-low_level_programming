9b568226bdd3e85ac661b6828afa7393e1b24c05
inioluwa-kidat001 <inioluwa-kidat001@gmail.com>
2022-10-19 19:20:38 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a an int
+ * @n:int to print its absolute
+ * Return:0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
