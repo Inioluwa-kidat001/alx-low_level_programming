46df9f03ac1c31b3ec923208c9145208ea2fc0cc
inioluwa-kidat001 <inioluwa-kidat001@gmail.com>
2022-10-19 22:31:56 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,18 +18,16 @@ void jack_bauer(void)
 			{
 				for (m = 0; m >= 9; m++)
 				{
-					if (i == 2 && j > 3
-							{break;
-							}
-_putchar(i + '0');
-_putchar(j + '0');
-_putchar(':');
-_putchar(k + '0');
-_putchar(m + '0');
-_putchar('\n');
-
-}
-}
-}
-}
-}
+					if (i == 2 && j > 3)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(m + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
