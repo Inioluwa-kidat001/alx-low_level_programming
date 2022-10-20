76919a56fc75c14cead5b272249042f236118ff2
inioluwa-kidat001 <inioluwa-kidat001@gmail.com>
2022-10-19 22:51:18 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds 2 int and returns
+ * the result
+ *@i:num one
+ *@j:num two
+ *
+ * Return:add num 1 and 2
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
