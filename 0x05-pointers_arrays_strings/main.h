3dc073c68b3e57e3d812edccb699cd17be5218ec
inioluwa-kidat001 <inioluwakolade2000@gmail.com>
2022-10-24 08:30:32 -0700

    resettt

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,8 +5,7 @@
 int _putchar (char c);
 void reset_ to_98(int *n);
 void swap_int (int *a, int *b);
-size t
-_strlen(const char *str);
+int _strlen(const char *str);
 void puts(char *str);
 void print_rev(char *s);
 void rev_string (char *s);
