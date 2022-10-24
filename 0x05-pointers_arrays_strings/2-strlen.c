0c1227af5e5399c94879daa86582321559d3e61f
inioluwa-kidat001 <inioluwakolade2000@gmail.com>
2022-10-24 16:15:54 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - finds lenght of a string
+ * @s: string pointer
+ * Return: returns lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int k = 0
+		/*increament up to when last char is NULL,\0*/
+		while (*(s + k) != 0)
+		{
+			k++;
+		}
+	return (k);
+}
