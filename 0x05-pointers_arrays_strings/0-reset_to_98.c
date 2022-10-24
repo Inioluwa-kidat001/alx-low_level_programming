37aebac8e61e2815f04617a22627e75574c96fda
inioluwa-kidat001 <inioluwa-kidat001@gmail.com>
2022-10-24 06:48:26 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets value of pointer to 98
+ * @n: pointer to reset to 98
+ *
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN H
+#define MAIN H
+#include <stdlib.h>
+
+int _putchar (char c);
+void reset_ to_98(int *n);
+void swap_int (int *a, int *b);
+size t
+_strlen(const char *str);
+void puts(char *str);
+void print_rev(char *s);
+void rev_string (char *s);
+void puts2(char *str);
+void puts_half (char *str);
+char *_ strcpy (char *dest, char *src);
+
+#endif
