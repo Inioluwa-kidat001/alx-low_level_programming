835e6dab1c31e344d5bd515c4de71c528cf61970
inioluwa-kidat001 <inioluwakolade2000@gmail.com>
2022-10-24 17:22:21 -0700

    putshalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of string
+ * @str: input string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int r = 0, len = 0, p;
+
+	while (str[r++])
+		len++;
+
+	if ((len % 2) == 0)
+		p = len / 2;
+
+	else
+		p = (len + 1) / 2;
+
+	for (r = j; r < len; r++)
+		_putchar(str[r]);
+
+	_putchar('\n');
+}
