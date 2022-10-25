aef6b4b75081fb3379873e001d5ad103118d2991
inioluwa-kidat001 <inioluwakolade2000@gmail.com>
2022-10-24 17:38:36 -0700

    print-array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints inputed list of elements
+ * @a: array of int
+ * @n: number of elements to print
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(",");
+	}
+	printf("\n");
+}
